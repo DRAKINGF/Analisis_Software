dcae35e2d293a84f12064e6881b018d23c9c3d5a
JHONPEREZ <107455171+DRAKINGF@users.noreply.github.com>
2022-10-17 21:17:46 -0500

    cambio main

--- main.php
+++ main.php
@@ -51,7 +51,7 @@ session_start();
             <li class="nav-item active">
                 <a class="nav-link" href="main.php">
                 <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Reporte</span></a>
+                    <span>Report</span></a>
             </li>
 
             <!-- Divider -->
