86c0c7af3050f439fa180ad3f255c305c8da9209
Giuseppe Tamayo <gtamayoc@ufpso.edu.co>
2022-11-06 22:32:50 -0500

    AJUSTE - Inicio de sesión.
    - Se ajusta la consulta del usuario en la tabla de acceso.

--- Modal/sesion.php
+++ Modal/sesion.php
@@ -7,49 +7,35 @@ session_start();
 $usuario=$_POST['user'];
 $clave=$_POST['pass'];
 
-//$query=("SELECT * FROM usuarios INNER JOIN acceso ON acceso.id_usuario=usuarios.id_usuario WHERE usuarios.documento_usu='$usuario' AND usuarios.clave_usu='$clave'");
-//$query=("SELECT * FROM acceso WHERE usuario='$usuario' AND password='$clave'");
-$query=("SELECT * FROM usuarios WHERE documento_usu='$usuario' AND clave_usu='$clave'  and estado_usu='SI' and id_cargo='2' ");
+$query=("SELECT * FROM usuarios INNER JOIN acceso ON usuarios.id_usuario=acceso.id_usuario 
+		WHERE usuarios.documento_usu='$usuario' AND usuarios.clave_usu='$clave' 
+		and estado_usu='SI'");
+
 $consulta=pg_query($conexion,$query);
+$id = pg_fetch_array($consulta);
 $cantidad=pg_num_rows($consulta);
-var_dump($consulta);
-$query2=("SELECT * FROM usuarios WHERE documento_usu='$usuario' AND clave_usu='$clave'  and estado_usu='SI' and id_cargo='1' ");
-$consulta2=pg_query($conexion,$query2);
-$cantidad2=pg_num_rows($consulta2);
-/*
-$consulta2 = pg_fetch_array($consulta);
-print_r($consulta2);
-*/
+var_dump($id);
+
 if($cantidad>0){
-	$id_empleado = pg_fetch_array($consulta);
+	$id_empleado = $id;
 	$id_Acces = $id_empleado['id_acceso'];
 	$_SESSION['nombre_usuario']=$id_empleado['id_usuario'];  
 	$user1=$id_empleado['nombre1_usu'];
 	$cargo=$id_empleado['id_cargo'];
 	$_SESSION['user_name']=$user1;
 	$_SESSION['user_cargo']=$cargo;
-	var_dump($_SESSION['user_name']);
-	header('Location:../tables2.php');  
+	// Hacer modificacion en la tabla de "cargos" o VALIDAR que el id_cargo = 1 corresponda al ADMINISTRADOR
+	// Y el id_cargo=2 corresponda al EMPLEADO
+	if($cargo=="2"){
+		//var_dump($_SESSION['user_name']);
+		header('Location:../tables2.php'); 
+	}else{
+		//var_dump($_SESSION['user_name']);
+		header('Location:../empleados2.php');  
+	} 
 }
 else{
 	echo "Datos incorrectos.";
 }
-if($cantidad2>0){
-	$id_empleado = pg_fetch_array($consulta2);
-	$id_Acces = $id_empleado['id_acceso'];
-	$_SESSION['nombre_usuario']=$id_empleado['id_usuario'];  
-	$user1=$id_empleado['nombre1_usu'];
-	$cargo=$id_empleado['id_cargo'];
-	$_SESSION['user_name']=$user1;
-	$_SESSION['user_cargo']=$cargo;
-	var_dump($_SESSION['user_name']);
-	header('Location:../empleados2.php');  
-}
-else{
-	echo "Datos incorrectos.";
-}
-
-
-
 
 ?>
